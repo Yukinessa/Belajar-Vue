83aeb7f963ef52fb64d7aea2ae98806c5a2efaa2
yukinessa <kun.amrin@gmail.com>
2020-11-10 15:59:41 +0700

    first

---
+++ src/components/CardAnime.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{anime.title}}</h1>
+    <img :src="anime.image_url" />
+    <h1>{{anime.score}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    anime: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+};
+</script>
+
+<style>
+</style>
